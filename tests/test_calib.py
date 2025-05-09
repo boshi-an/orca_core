5ed047efa6761c38056c66d7d8ad0dfdee304aea
boshi-an <boshi_an2stu.pku.edu.cn>
2025-05-09 21:10:43 +0200

    fixed bugs, added testing scripts

---
+++ tests/test_calib.py
@@ -0,0 +1,24 @@
+from orca_core import OrcaHand
+import time
+
+
+if __name__ == "__main__":
+    # Example usage:
+    try :
+        hand = OrcaHand()
+        status = hand.connect()
+        hand.enable_torque()
+        hand.calibrate()
+
+        # Set the desired joint positions to 0
+        hand.set_joint_pos({joint: 0 for joint in hand.joint_ids})
+        time.sleep(1)
+        # Set the desired joint positions to 90 degrees
+        hand.set_joint_pos({joint: 90 for joint in {"index_mcp", "middle_mcp", "ring_mcp","pinky_mcp"}})
+        time.sleep(0.1)
+        hand.set_joint_pos({joint: 45 for joint in {"index_mcp", "middle_mcp", "ring_mcp","pinky_mcp"}})
+        time.sleep(2)
+    finally:
+        hand.disable_torque()
+        hand.disconnect()
+        
